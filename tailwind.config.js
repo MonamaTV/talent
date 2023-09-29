0e7a9581e3caaa7669e303ca017dbc8675e7a033
Tadima <19066791722k23@bootcamp.wethinkcode.co.za>
2023-09-30 01:28:22 +0200

    added color gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "primary-color": "#00c9a7",
+        "secondary-color": "#4d8076",
+        "tertiary-color": "#845ec2",
+        "text-color": "c4fcef",
       },
     },
   },
   plugins: [],
-}
+};
